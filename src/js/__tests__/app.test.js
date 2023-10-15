4b245c95237abc353819ef4ef4424d3f01a1ee80
NikitaKST <8172535@gmail.com>
2023-10-15 19:57:50 +0500

    Task complete

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,55 @@
+import getSpetial from '../app';
+
+test('test function getSpetial', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  const res = getSpetial(character);
+  expect(res).toEqual(result);
+});
+
+test('test function getSpetial without special', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+  };
+  const result = [];
+  const res = getSpetial(character);
+  expect(res).toEqual(result);
+});
